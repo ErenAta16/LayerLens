0ab414510a6b5bda7f57e5fbc576fd786cca7166
ErenAta16 <erena6466@gmail.com>
2026-01-12 14:54:40 +0300

    Export validation and models submodules in layerlens.__init__ for type checking

--- layerlens/__init__.py
+++ layerlens/__init__.py
@@ -8,5 +8,11 @@ Python interfaces for core algorithms that will be accelerated with Cython.
 
 from .config import ProfilingConfig, OptimizationConfig, LatencyProfile
 
+# Import submodules to make them available for type checking
+from . import validation  # noqa: F401
+from . import models  # noqa: F401
+from . import cli  # noqa: F401
+from . import pipeline  # noqa: F401
+
 __all__ = ["ProfilingConfig", "OptimizationConfig", "LatencyProfile"]
 
